57949d752aedf185327ec19785f4f84e6bb072e6
skudinva <skudinva@gmail.com>
2024-11-25 21:54:00 +0700

    ++

--- project/libs/shared/core/src/lib/types/post.interface.ts
+++ project/libs/shared/core/src/lib/types/post.interface.ts
@@ -1,17 +1,20 @@
 import { PostState } from './post-state.enum';
 import { PostType } from './post-type.enum';
+import { Tag } from './tag.interface';
 
 export interface Post<T extends PostType> {
   id?: string;
   postType: T;
   authorId: string;
   isRepost: boolean;
-  originAuthorId: string;
-  originPostId: string;
-  tags?: string[];
+  originAuthorId?: string;
+  originPostId?: string;
+  tags?: Tag[];
   state: PostState;
   createDate: Date;
   publicDate: Date;
+  likesCount: number;
+  commentsCount: number;
 }
 
 export interface VideoPost extends Post<PostType.Video> {
