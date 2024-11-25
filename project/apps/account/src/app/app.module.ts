2255f1f68543ac82427ac055248866e02eb9b2d3
Vladimir Skudin <skudinva@yandex.ru>
2024-11-25 16:33:52 +0700

    ++

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from '@project/authentication';
+import { BlogUserModule } from '@project/blog-user';
 
 @Module({
-  imports: [],
+  imports: [BlogUserModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
