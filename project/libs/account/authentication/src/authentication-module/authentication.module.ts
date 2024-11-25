60dc89383d2c879e8b3b597b72f7afb018b9491c
Vladimir Skudin <skudinva@yandex.ru>
2024-11-25 15:47:53 +0700

    ++

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BlogUserModule } from '@project/blog-user';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
+  imports: [BlogUserModule],
 })
 export class AuthenticationModule {}
