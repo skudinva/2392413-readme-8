3d44a341b4c2d31892c6f90a7cf645b07ea92132
Vladimir Skudin <skudinva@yandex.ru>
2024-11-25 14:38:37 +0700

    ++

--- project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
@@ -1,11 +1,11 @@
 import { AuthUser, Entity, StorableEntity } from '@project/shared/core';
 
 export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
-  public email?: string;
-  public name?: string;
-  public avatar?: string;
-  public registerDate?: Date;
-  public passwordHash?: string;
+  public email: string;
+  public name: string;
+  public avatar: string;
+  public registerDate: Date;
+  public passwordHash: string;
 
   constructor(user?: AuthUser) {
     super();
@@ -15,22 +15,26 @@ export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
     if (!user) {
       return;
     }
-    this.id = user.id ?? '';
-    this.email = user.email;
-    this.name = user.name;
-    this.avatar = user.avatar ?? '';
-    this.registerDate = user.registerDate;
-    this.passwordHash = user.passwordHash;
+    const { id, email, name, avatar, registerDate, passwordHash } = user;
+
+    this.id = id ?? '';
+    this.email = email;
+    this.name = name;
+    this.avatar = avatar ?? '';
+    this.registerDate = registerDate;
+    this.passwordHash = passwordHash;
   }
 
   toPOJO(): AuthUser {
+    const { id, email, name, avatar, registerDate, passwordHash } = this;
+
     return {
-      id: this.id,
-      email: this.email,
-      name: this.name,
-      avatar: this.avatar,
-      registerDate: this.registerDate,
-      passwordHash: this.passwordHash,
+      id,
+      email,
+      name,
+      avatar,
+      registerDate,
+      passwordHash,
     };
   }
 }
