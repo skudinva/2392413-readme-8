c9736b710ee723bbff61fce4efaaafa6ab1f38a2
Vladimir Skudin <skudinva@yandex.ru>
2024-11-25 15:35:59 +0700

    ++

---
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.repository.ts
@@ -0,0 +1,9 @@
+import { BaseMemoryRepository } from '@project/data-access';
+import { BlogUserEntity } from './blog-user.entity';
+import { BlogUserFactory } from './blog-user.factory';
+
+export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {
+  constructor(entityFactory: BlogUserFactory) {
+    super(entityFactory);
+  }
+}
