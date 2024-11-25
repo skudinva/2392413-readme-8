e9a86d0174f684aba0897d8d5cbf729c94fad97d
Vladimir Skudin <skudinva@yandex.ru>
2024-11-25 16:19:52 +0700

    ++

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { BlogUserModule } from '@project/blog-user';
+
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
-  controllers: [AuthenticationController],
-  providers: [AuthenticationService],
   imports: [BlogUserModule],
+  controllers: [AuthenticationController],
+  providers: [AuthenticationService]
 })
 export class AuthenticationModule {}
--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,7 +1,70 @@
-import { Injectable } from '@nestjs/common';
-import { BlogUserRepository } from '@project/blog-user';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { BlogUserEntity, BlogUserRepository } from '@project/blog-user';
+import { AuthUser } from '@project/shared/core';
+import dayjs from 'dayjs';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { LoginUserDto } from '../dto/login-user.dto';
+import {
+  AUTH_USER_EXISTS,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+} from './authentication.constant';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private readonly blogUserRepository: BlogUserRepository) {}
+
+  public async register(dto: CreateUserDto): Promise<BlogUserEntity> {
+    const { email, name, avatar, password } = dto;
+
+    const blogUser: AuthUser = {
+      email,
+      name,
+      avatar,
+      registerDate: dayjs().toDate(),
+      passwordHash: '',
+    };
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+
+    this.blogUserRepository.save(userEntity);
+
+    return userEntity;
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const { email, password } = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    if (!(await existUser.comparePassword(password))) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    const user = await this.blogUserRepository.findById(id);
+
+    if (!user) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    return user;
+  }
 }
