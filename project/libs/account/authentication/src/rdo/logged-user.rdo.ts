348315c43f6e14420268bddfb89f4dcec63c89a9
skudinva <skudinva@gmail.com>
2024-11-30 18:17:59 +0700

    feat: Integrate Swagger for API documentation

--- project/libs/account/authentication/src/rdo/logged-user.rdo.ts
+++ project/libs/account/authentication/src/rdo/logged-user.rdo.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class LoggedUserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '134ce8babd-cc30-4805-9b12-d9420398e7c5',
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local',
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'Access token',
+    example: 'user@user.local',
+  })
   @Expose()
   public accessToken: string;
 }
