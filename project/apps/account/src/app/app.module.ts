1cdfaf1f67c71dee8c38e97a5fa4184a1318588c
skudinva <skudinva@gmail.com>
2024-12-02 19:50:01 +0700

    feat: Integrate MongoDB config with Mongoose

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthenticationModule } from '@project/authentication';
 import { BlogUserModule } from '@project/blog-user';
-import { AccountConfigModule } from '@project/config';
+import { AccountConfigModule, getMongooseOptions } from '@project/config';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule, AccountConfigModule],
+  imports: [
+    BlogUserModule,
+    AuthenticationModule,
+    AccountConfigModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
+  ],
   controllers: [],
   providers: [],
 })
