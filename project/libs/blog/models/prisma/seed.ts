26033a655a9fc55e7abce4997b6e6e50447df002
skudinva <skudinva@gmail.com>
2024-12-08 21:45:47 +0700

    feat: Add script to seed database with mock posts

---
+++ project/libs/blog/models/prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+function getPosts() {
+  return [];
+}
+
+async function seedDb(prismaClient: PrismaClient) {
+  const mockPosts = getPosts();
+  for (const post of mockPosts) {
+    await prismaClient.post.create({});
+  }
+
+  console.info('🤘️ Database was filled');
+}
+
+async function bootstrap() {
+  const prismaClient = new PrismaClient();
+
+  try {
+    await seedDb(prismaClient);
+    globalThis.process.exit(0);
+  } catch (error: unknown) {
+    console.error(error);
+    globalThis.process.exit(1);
+  } finally {
+    await prismaClient.$disconnect();
+  }
+}
+
+bootstrap();
