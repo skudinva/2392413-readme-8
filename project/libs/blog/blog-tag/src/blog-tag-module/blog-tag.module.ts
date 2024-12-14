011df8e5849a3e4c0f12ab73a42a680c03fc49af
skudinva <skudinva@gmail.com>
2024-12-14 20:40:30 +0700

    Поправил импорты

--- project/libs/blog/blog-tag/src/blog-tag-module/blog-tag.module.ts
+++ project/libs/blog/blog-tag/src/blog-tag-module/blog-tag.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaClientModule } from '@project/blog-models';
 import { BlogTagFactory } from './blog-tag.factory';
 import { BlogTagRepository } from './blog-tag.repository';
+import { BlogTagService } from './blog-tag.service';
 
 @Module({
-  providers: [BlogTagRepository, BlogTagFactory],
-  exports: [BlogTagRepository],
+  imports: [PrismaClientModule],
+  providers: [BlogTagRepository, BlogTagFactory, BlogTagService],
+  exports: [BlogTagService],
 })
 export class BlogTagModule {}
