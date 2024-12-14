138d0a053e053fbb531446e58f00db787ba3cbe1
skudinva <skudinva@gmail.com>
2024-12-14 21:34:03 +0700

    ++

--- project/libs/blog/blog-post/src/blog-post-module/blog-post.module.ts
+++ project/libs/blog/blog-post/src/blog-post-module/blog-post.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { BlogLikeModule } from '@project/blog-like';
+import { BlogTagModule } from '@project/blog-tag';
 import { BlogPostFactory } from './blog-post.factory';
 import { BlogPostRepository } from './blog-post.repository';
+import { BlogPostService } from './blog-post.service';
 
 @Module({
-  providers: [BlogPostRepository, BlogPostFactory],
-  exports: [BlogPostRepository],
+  imports: [BlogTagModule, BlogLikeModule],
+  providers: [BlogPostService, BlogPostRepository, BlogPostFactory],
+  exports: [BlogPostService],
 })
 export class BlogPostModule {}
