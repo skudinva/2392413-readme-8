bdf9697e3d85ea72b279aea090e7d9c50c8140cc
skudinva <skudinva@gmail.com>
2024-12-18 13:34:20 +0700

    Добавит функцию getJwtOptions. WIP

--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -3,3 +3,4 @@ export { default as applicationConfig } from './configurations/app.config';
 export { default as jwtConfig } from './configurations/jwt.config';
 export { default as dbConfig } from './configurations/mongo.config';
 export { getMongooseOptions } from './configurations/mongodb/get-mongoose-options';
+export { getJwtOptions } from './get-jwt-options';
