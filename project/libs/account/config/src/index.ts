e453cecf5e4d02b033d42b2e6cc53cfa116c4a77
skudinva <skudinva@gmail.com>
2024-12-18 13:30:01 +0700

    Добавит настройки для JWT.

--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -1,4 +1,5 @@
 export { AccountConfigModule } from './account-config.module';
 export { default as applicationConfig } from './configurations/app.config';
+export { default as jwtConfig } from './configurations/jwt.config';
 export { default as dbConfig } from './configurations/mongo.config';
 export { getMongooseOptions } from './configurations/mongodb/get-mongoose-options';
