b421a93889976f863b77635c6d20f7b232737cde
skudinva <skudinva@gmail.com>
2024-12-23 22:51:45 +0700

    Добавит контроллер FileUploaderController. WIP

---
+++ project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import 'multer';
+import { FileUploaderService } from './file-uploader.service';
+
+@Controller('files')
+export class FileUploaderController {
+  constructor(private readonly fileUploaderService: FileUploaderService) {}
+
+  @Post('/upload')
+  @UseInterceptors(FileInterceptor('file'))
+  public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileUploaderService.saveFile(file);
+  }
+}
