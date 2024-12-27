3a57d48c16a3de4ff9721fd632721f3c4e222a2d
skudinva <skudinva@gmail.com>
2024-12-27 11:32:31 +0700

    Подключит HttpModule в app.module.ts.

--- project/apps/api/src/app/app.module.ts
+++ project/apps/api/src/app/app.module.ts
@@ -1,7 +1,14 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { HTTP_CLIENT_MAX_REDIRECTS, HTTP_CLIENT_TIMEOUT } from './app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.register({
+      timeout: HTTP_CLIENT_TIMEOUT,
+      maxRedirects: HTTP_CLIENT_MAX_REDIRECTS,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
