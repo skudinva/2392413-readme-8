7f832df988778f0b4fb47899481578f25fbe7126
skudinva <skudinva@gmail.com>
2024-12-27 12:45:56 +0700

    Добавит обработчик создания новой публикации в блоге.

--- project/apps/api/src/app/app.module.ts
+++ project/apps/api/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { HTTP_CLIENT_MAX_REDIRECTS, HTTP_CLIENT_TIMEOUT } from './app.config';
+import { BlogController } from './blog.controller';
 import { CheckAuthGuard } from './guards/check-auth.guard';
 import { UsersController } from './users.controller';
 
@@ -11,7 +12,7 @@ import { UsersController } from './users.controller';
       maxRedirects: HTTP_CLIENT_MAX_REDIRECTS,
     }),
   ],
-  controllers: [UsersController],
+  controllers: [UsersController, BlogController],
   providers: [CheckAuthGuard],
 })
 export class AppModule {}
--- project/libs/blog/blog-post/src/index.ts
+++ project/libs/blog/blog-post/src/index.ts
@@ -2,4 +2,6 @@ export { BlogPostEntity } from './blog-post-module/blog-post.entity';
 export { BlogPostModule } from './blog-post-module/blog-post.module';
 export { BlogPostRepository } from './blog-post-module/blog-post.repository';
 export { BlogPostService } from './blog-post-module/blog-post.service';
+export { CreatePostDto } from './blog-post-module/dto/create-post.dto';
+export { PostExtraPropertyDto } from './blog-post-module/dto/post-extra-property.dto';
 export { BlogPostRdo } from './blog-post-module/rdo/blog-post.rdo';
