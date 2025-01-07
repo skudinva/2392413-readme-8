29f80754e9cc03133f4f95a5676b4b92eb56a340
skudinva <skudinva@gmail.com>
2025-01-07 16:10:31 +0700

    feat: Integrate Swagger for API documentation

--- project/apps/api/src/main.ts
+++ project/apps/api/src/main.ts
@@ -1,12 +1,21 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { RequestIdInterceptor } from '@project/interceptors';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  const config = new DocumentBuilder()
+    .setTitle('Readme app')
+    .setDescription('Readme app API')
+    .setVersion('1.0')
+    .build();
+
   app.setGlobalPrefix(globalPrefix);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
   app.useGlobalInterceptors(new RequestIdInterceptor());
   const port = process.env.PORT || 3000;
   await app.listen(port);
