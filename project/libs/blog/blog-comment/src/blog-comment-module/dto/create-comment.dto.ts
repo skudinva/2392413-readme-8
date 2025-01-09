c36712f09f771f3c1b897d6791949bf892c6d5bb
skudinva <skudinva@gmail.com>
2025-01-09 16:35:06 +0700

    correct dto

--- project/libs/blog/blog-comment/src/blog-comment-module/dto/create-comment.dto.ts
+++ project/libs/blog/blog-comment/src/blog-comment-module/dto/create-comment.dto.ts
@@ -1,12 +1,23 @@
-import { IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId, IsNotEmpty, IsString, Length } from 'class-validator';
 import { BlogCommentValidateMessage } from '../blog-comment.constant';
 
 export class CreateCommentDto {
   @IsString()
   @IsNotEmpty({ message: BlogCommentValidateMessage.MessageIsEmpty })
+  @ApiProperty({
+    description: 'Comment message',
+    example: 'Some comment for post',
+  })
+  @IsString()
+  @Length(10, 300)
   public message!: string;
 
   @IsString()
   @IsMongoId({ message: BlogCommentValidateMessage.InvalidID })
+  @ApiProperty({
+    description: 'User Id',
+    example: '888aef3b7eadb76365f3c2cb',
+  })
   public userId!: string;
 }
--- project/libs/blog/blog-tag/src/blog-tag-module/dto/create-tag.dto.ts
+++ project/libs/blog/blog-tag/src/blog-tag-module/dto/create-tag.dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length } from 'class-validator';
+
 export class CreateTagDto {
+  @ApiProperty({
+    description: 'Tag for the post',
+    example: 'sometag',
+  })
+  @IsString()
+  @Length(3, 10)
   public title!: string;
 }
