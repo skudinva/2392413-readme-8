bd123d051996daa08f565e08ea9abba620949f09
skudinva <skudinva@gmail.com>
2025-01-12 19:59:09 +0700

    feat: Add file upload and new post retrieval endpoints

--- project/apps/api/src/app/blog.controller.ts
+++ project/apps/api/src/app/blog.controller.ts
@@ -2,12 +2,26 @@ import { HttpService } from '@nestjs/axios';
 import {
   Body,
   Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
   Post,
+  Req,
+  UploadedFile,
   UseFilters,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { CreatePostDto } from '@project/blog-post';
+import { createUrlForFile } from '@project/helpers';
+import { InjectUserIdInterceptor } from '@project/interceptors';
+import { File } from '@project/shared/core';
+import FormData from 'form-data';
+import 'multer';
+import * as url from 'node:url';
 import { ApplicationServiceURL } from './app.config';
 import { AxiosExceptionFilter } from './filters/axios-exception.filter';
 import { CheckAuthGuard } from './guards/check-auth.guard';
@@ -19,12 +33,60 @@ export class BlogController {
 
   @UseGuards(CheckAuthGuard)
   @UseInterceptors(UseInterceptors)
+  @UseInterceptors(InjectUserIdInterceptor)
+  @UseInterceptors(FileInterceptor('file'))
   @Post('/')
-  public async create(@Body() dto: CreatePostDto) {
+  public async create(
+    @Body() dto: CreatePostDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000000 }),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+        ],
+        fileIsRequired: false,
+      })
+    )
+    file?: Express.Multer.File
+  ) {
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', new Blob([file.buffer]), file.originalname);
+      const { data: fileMetaData } = await this.httpService.axiosRef.post<File>(
+        `${ApplicationServiceURL.File}/api/files/upload`,
+        formData,
+        {
+          headers: formData.getHeaders(),
+        }
+      );
+      dto.extraProperty.photo = createUrlForFile(
+        fileMetaData,
+        ApplicationServiceURL.File
+      );
+    }
+
     const { data } = await this.httpService.axiosRef.post(
       `${ApplicationServiceURL.Blog}/`,
       dto
     );
+    return data;
+  }
+
+  @Get('/')
+  public async getPosts(@Req() req: Request) {
+    const { data } = await this.httpService.axiosRef.get(
+      `${ApplicationServiceURL.Blog}?${url.parse(req.url).query}`
+    );
+
+    return data;
+  }
+
+  @Get('/:id')
+  public async getPost(@Param('id') id: string) {
+    const { data } = await this.httpService.axiosRef.get(
+      `${ApplicationServiceURL.Blog}/${id}`
+    );
+
     return data;
   }
 }
