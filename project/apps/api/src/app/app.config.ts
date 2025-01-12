6a948482a780087fe60757eeedd7c1e1855e6460
skudinva <skudinva@gmail.com>
2025-01-12 19:01:20 +0700

    feat: Add user registration with avatar upload feature

--- project/apps/api/src/app/app.config.ts
+++ project/apps/api/src/app/app.config.ts
@@ -1,6 +1,7 @@
 export enum ApplicationServiceURL {
   Users = 'http://localhost:3000/api/auth',
   Blog = 'http://localhost:3001/api/posts',
+  File = 'http://localhost:3002',
 }
 
 export const HTTP_CLIENT_MAX_REDIRECTS = 5;
--- project/apps/api/src/app/users.controller.ts
+++ project/apps/api/src/app/users.controller.ts
@@ -1,13 +1,68 @@
 import { HttpService } from '@nestjs/axios';
-import { Body, Controller, Post, Req, UseFilters } from '@nestjs/common';
-import { LoginUserDto } from '@project/authentication';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  Req,
+  UploadedFile,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateUserDto, LoginUserDto } from '@project/authentication';
+import { createUrlForFile } from '@project/helpers';
+import { File } from '@project/shared/core';
+import FormData from 'form-data';
 import { ApplicationServiceURL } from './app.config';
 import { AxiosExceptionFilter } from './filters/axios-exception.filter';
+
+const DEFAULT_AVATAR_PATH = `${ApplicationServiceURL.File}/static/default-avatar.jpg`;
 
 @Controller('users')
 @UseFilters(AxiosExceptionFilter)
 export class UsersController {
   constructor(private readonly httpService: HttpService) {}
+
+  @Post('register')
+  @UseInterceptors(FileInterceptor('avatar'))
+  public async create(
+    @Body() dto: CreateUserDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 500000 }),
+          new FileTypeValidator({ fileType: '.(jpg|jpeg|png)' }),
+        ],
+        fileIsRequired: false,
+      })
+    )
+    avatar?: Express.Multer.File
+  ) {
+    dto.avatar = DEFAULT_AVATAR_PATH;
+
+    if (avatar) {
+      const formData = new FormData();
+      formData.append('file', new Blob([avatar.buffer]), avatar.originalname);
+      const { data: fileMetaData } = await this.httpService.axiosRef.post<File>(
+        `${ApplicationServiceURL.File}/api/files/upload`,
+        formData,
+        {
+          headers: formData.getHeaders(),
+        }
+      );
+      dto.avatar = createUrlForFile(fileMetaData, ApplicationServiceURL.File);
+    }
+
+    const { data } = await this.httpService.axiosRef.post(
+      `${ApplicationServiceURL.Users}/register`,
+      dto
+    );
+
+    return data;
+  }
 
   @Post('login')
   public async login(@Body() loginUserDto: LoginUserDto) {
