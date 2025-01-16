fc3dfeba704d92982952292c9365e3442700d3a3
skudinva <skudinva@gmail.com>
2025-01-16 11:29:56 +0700

    ++

--- project/libs/blog/blog-post/src/blog-post-module/dto/user-id.dto.ts
+++ project/libs/blog/blog-post/src/blog-post-module/dto/user-id.dto.ts
@@ -3,7 +3,7 @@ import { IsMongoId } from 'class-validator';
 
 export class UserIdDto {
   @ApiProperty({
-    description: "Id of the like's author",
+    description: 'userId',
     example: '669aef3b7eadb26966f3c2cb',
   })
   @IsMongoId({ message: 'UserId should be valid MongoId' })
