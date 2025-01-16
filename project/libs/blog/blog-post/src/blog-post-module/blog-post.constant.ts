bf1cc3786d13abf0a80520b463c9da55adb282ac
skudinva <skudinva@gmail.com>
2025-01-16 12:26:53 +0700

    unused constant

--- project/libs/blog/blog-post/src/blog-post-module/blog-post.constant.ts
+++ project/libs/blog/blog-post/src/blog-post-module/blog-post.constant.ts
@@ -1,6 +1,5 @@
 import { SortDirection, SortType } from '@project/shared/core';
 
-export const MAX_TAGS_COUNT = 8;
 export const DEFAULT_POST_COUNT_LIMIT = 25;
 export const DEFAULT_SORT_DIRECTION = SortDirection.Desc;
 export const DEFAULT_SORT_TYPE = SortType.DATE;
