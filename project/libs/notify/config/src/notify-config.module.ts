38365b6a4181a59733053328cc61047824ce03fe
skudinva <skudinva@gmail.com>
2025-01-19 20:55:44 +0700

    refactor: Remove env file path from config modules

--- project/libs/notify/config/src/notify-config.module.ts
+++ project/libs/notify/config/src/notify-config.module.ts
@@ -2,15 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import notifyConfig from './notify.config';
 
-const ENV_FILE_PATH = 'apps/notify/notify.env';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
       load: [notifyConfig],
-      envFilePath: ENV_FILE_PATH,
     }),
   ],
 })
